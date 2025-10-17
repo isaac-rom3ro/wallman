8657d3b79eacb6276a4ad18c4731e9d4d39b51b3
isaac-rom3ro <is4ac.romero@outlook.com>
2025-10-17 12:10:21 -0300

    feat/package verification

--- script.sh
+++ script.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-echo RUNNING...
 
-PACKAGE="feh"
-BG=$1
+background_path=""
+lib_background="feh"
 
-if apt list --installed 2>/dev/null | grep -q "^$PACKAGE/"; then
-	feh --bg-scale $BG
+echo -n "Please provide the path of your background: "
+read background_path
+
+function check_package_manager()
+{
+  if command -v apt >/dev/null 2>&1; then
+    echo "apt"
+  fi
+}
+
+package_manager=$(check_package_manager)
+
+if $package_manager list --installed 2>/dev/null | grep -q "^$PACKAGE/"; then
+  echo "$package_manager is installed"
 else
-	echo "not installed"
+  echo "not installed"
 fi
-
-
-
-
